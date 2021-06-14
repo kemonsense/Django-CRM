393f3b03247a279c08afaeaa1b89325e5f0ec163
kemonsense <qzjaman@gmail.com>
2021-06-14 15:35:42 +0600

    Initial commit

---
+++ leads/views.py
@@ -0,0 +1,59 @@
+
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.views import generic
+from .models import *
+from .forms import *
+
+# CRUD+L - Create, Retrieve, Update and Delete + List
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
+
+
+class LandingPageView(generic.TemplateView):
+    template_name = "landing.html"
+
+
+class LeadListView(LoginRequiredMixin, generic.ListView):
+    template_name = "leads/lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+
+
+class LeadDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+
+class LeadCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all()
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
